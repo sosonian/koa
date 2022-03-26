d110ad33a8371e321a7f8b58a924bd10085a9906
sosonian <sosonian@gmail.com>
2022-03-26 12:20:01 +0000

    first commit

---
+++ companyList.js
@@ -0,0 +1,14 @@
+module.exports = {
+    CompanyList : [
+        {Name:'Apple', Symbol:'APPL'},
+        {Name:'Amazon', Symbol:'AMZN'},
+        {Name:'MicroSoft',Symbol:'MSFT'},
+        {Name:'Alphabet',Symbol:'GOOGL'},
+        {Name:'Tesla', Symbol:'TSLA'},
+        {Name:'TSMC', Symbol:'TSM'},
+        {Name:'Coca Cola',Symbol:'KO'},
+        {Name:'Meta', Symbol:'FB'},
+        {Name:'Lockheed Martin', Symbol:'LMT'},
+        {Name:'General Motors', Symbol:'GM'}
+    ]
+}
---
+++ loginInfo.js
@@ -0,0 +1,11 @@
+module.exports = {
+    MongoAtlas:{
+        Url:'cluster0.ygejl.mongodb.net',
+        Account:'sosonian',
+        Password:'IRock1234',
+        Database:'StockTrading'
+    },
+    Finnhub:{
+        Token:'c8up83qad3ibdduenmsg'
+    }
+}
