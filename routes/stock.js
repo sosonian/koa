00caf64b7ede4737d4eecbd1a1369f59265c3947
sosonian <sosonian@gmail.com>
2022-03-28 04:50:32 +0000

    commit

--- routes/stock.js
+++ routes/stock.js
@@ -76,6 +76,7 @@ module.exports = function(dbConn, router) {
         let symbol = ctx.request.body.symbol
         let tradeUnit = ctx.request.body.tradeUnit
         let method = ctx.request.body.method
+        let fBalance
 
         let accountID = ctx.response.get('User-AccountID')
         
@@ -86,8 +87,6 @@ module.exports = function(dbConn, router) {
         }
         else
         {
-            console.log(tradeUnit)
-            console.log(isNaN(tradeUnit))
             let falseToken = false
             if(symbol === ''){falseToken = true}
             if(isNaN(tradeUnit)){falseToken = true}
@@ -100,33 +99,53 @@ module.exports = function(dbConn, router) {
             }
             else
             {
-                let rInfo = await getAccountBalanceAndStockPrice(accountID)
+                let rInfo = await getAccountBalanceAndStockPrice(accountID, symbol)
                 if(rInfo[0].status === 200 && rInfo[1].status ==200)
                 {
                     let balance = rInfo[0].msg.balance
                     let holdingStock = rInfo[0].msg.holdingStock
-                    let companyPrice = rInfo[1].msg.companyPrice
-                    let targetStock = {ticker:symbol, holdingUnit:0}
-                    
+                    let currentPrice = rInfo[1].msg.currentPrice
 
                     if(method === 'buy'){
                         let fHoldingStock = updateOrInsert(holdingStock,symbol, tradeUnit, true)
-                        if(balance < (tradeUnit*companyPrice)+10)
+                        if(balance < (tradeUnit*currentPrice)+10)
                         {
                             ctx.status = 200
                             ctx.body = "Balance is not enough, transaction cancelled"
                         }
                         else
                         {
-                            let fBalance = balance - 10 - (tradeUnit*companyPrice)
+                            fBalance = balance - 10 - (tradeUnit*currentPrice)
                             let fResult =  updateAccountBalanceAndStockPrice (accountID, fBalance, fHoldingStock)
+
+                            if(fResult.status = 200)
+                            {
+                                ctx.status = 200
+                                ctx.body = "Transacated Success !"
+                            }
+                            else
+                            {
+                                ctx.status = ffResult.status
+                                ctx.body = fResult.msg
+                            }
                         }
                     }
                     else if(method === 'sell'){
                         let fHoldingStock = updateOrInsert(holdingStock,symbol, tradeUnit, false)
                         if(fHoldingStock)
                         {
-                            let fResult =  updateAccountBalanceAndStockPrice (accountID, fBalance, fHoldingStock, )
+                            fBalance = balance + (tradeUnit*currentPrice)
+                            let fResult =  updateAccountBalanceAndStockPrice (accountID, fBalance, fHoldingStock)
+                            if(fResult.status = 200)
+                            {
+                                ctx.status = 200
+                                ctx.body = "Transacated Success !"
+                            }
+                            else
+                            {
+                                ctx.status = ffResult.status
+                                ctx.body = fResult.msg
+                            }
                         }
                         else
                         {
@@ -137,8 +156,21 @@ module.exports = function(dbConn, router) {
                 }
                 else
                 {
+                    let msg = ''
+                    if(rInfo[0].status !== 200 )
+                    {
+                        msg = msg + 'Fetch Account Failed!'
+                    }
+
+                    if(rInfo[1].status !== 200 )
+                    {
+                        msg = msg + 'Fetch Stock Info Failed!'
+                    }
+
+                    msg = msg + " Could't solve transaction"
+
                     ctx.status = 404
-                    ctx.body = "Data not found, couldn't solve transcation"
+                    ctx.body = msg
                 }
             }
 
@@ -196,7 +228,7 @@ module.exports = function(dbConn, router) {
 
     async function updateAccountBalanceAndStockPrice (accountID, fBalance, fHoldingStock) {
         
-        let accountUpdateResult = new Promise(async(resolve,reject)=>{
+        //let accountUpdateResult = new Promise(async(resolve,reject)=>{
             let aResult = await dbConn.then(async(conn)=>{
                 try {
                     let bResult = await conn.db('StockTrading').collection('Account').updateOne({accountID:accountID},{$set:{balance:fBalance,holdingStock:fHoldingStock}})
@@ -215,37 +247,39 @@ module.exports = function(dbConn, router) {
                     return {status:500, msg:'db error'}
                 }
             })
-            resolve(aResult)
-        })
+            //resolve(aResult)
+        //})
 
-        let companyPrice = new Promise(async(resolve,reject)=>{
-            let qResult = await dbConn.then(async(conn)=>{
-                try {
-                    let result = await conn.db('StockTrading').collection('CompanyProfile').findOne({"ticker":symbol})
-                    if(result && result !== null)
-                    {
-                        return {status:200, msg:result}
-                    }
-                    else
-                    {
-                        return {status:404, msg:'None Company Profile'}
-                    }
-                }
-                catch (e){
-                    console.log(e)
-                    return {status:500, msg:'db error'}
-                }
-            })
+        // let companyPrice = new Promise(async(resolve,reject)=>{
+        //     let qResult = await dbConn.then(async(conn)=>{
+        //         try {
+        //             let result = await conn.db('StockTrading').collection('CompanyProfile').findOne({"ticker":symbol})
+        //             if(result && result !== null)
+        //             {
+        //                 return {status:200, msg:result}
+        //             }
+        //             else
+        //             {
+        //                 return {status:404, msg:'None Company Profile'}
+        //             }
+        //         }
+        //         catch (e){
+        //             console.log(e)
+        //             return {status:500, msg:'db error'}
+        //         }
+        //     })
             
-            resolve(qResult)
-        })
+        //     resolve(qResult)
+        // })
 
-        let [aData, cPrice] = await Promise.all([account,companyPrice])       
-        return [aData, cPrice]      
+        // let [aData, cPrice] = await Promise.all([account,companyPrice])       
+        // return [aData, cPrice]    
+        return   aResult
     }
 
     function updateOrInsert(tArray, symbol, tradeUnit, buy){
         let fArray = []
+        
         if(tArray.length>0)
         {
             let existToken = false
@@ -254,16 +288,16 @@ module.exports = function(dbConn, router) {
                 if(s.ticker === symbol){
                     if(buy)
                     {
-                        tS.holdingUnit = s.holdingUnit+tradeUnit
+                        tS.holdingUnit = Number(s.holdingUnit)+Number(tradeUnit)
                     }
                     else
                     {
-                        if(s.holdingUnit-tradeUnit < 0)
+                        if(Number(s.holdingUnit)-Number(tradeUnit) < 0)
                         {
                             return false
                         }
 
-                        tS.holdingUnit = s.holdingUnit-tradeUnit
+                        tS.holdingUnit = Number(s.holdingUnit)-Number(tradeUnit)
                         
                     }
                     existToken = true
@@ -276,7 +310,7 @@ module.exports = function(dbConn, router) {
             {
                 if(buy)
                 {
-                    fArray.push({ticker:symbol,holdingUnit:tradeUnit})
+                    fArray.push({ticker:symbol,holdingUnit:Number(tradeUnit)})
                 }
                 else
                 {
@@ -288,7 +322,7 @@ module.exports = function(dbConn, router) {
         {
             if(buy)
             {
-                fArray.push({ticker:symbol,holdingUnit:tradeUnit})
+                fArray.push({ticker:symbol,holdingUnit:Number(tradeUnit)})
             }
             else
             {
